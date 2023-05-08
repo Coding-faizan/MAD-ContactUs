b75c803a28afb78dc78b4d12c26777ef9ee7e7cb
Faizan <>
2023-05-08 19:26:21 +0500

    added-back-button

--- app/src/main/java/com/example/contactus/ContactUs.java
+++ app/src/main/java/com/example/contactus/ContactUs.java
@@ -2,13 +2,14 @@ package com.example.contactus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 
 public class ContactUs extends AppCompatActivity {
 
-    private Button backButton;
     private ImageView backButtonImageview;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +19,16 @@ public class ContactUs extends AppCompatActivity {
         setContentView(R.layout.activity_contact_us);
 
         backButtonImageview = (ImageView) findViewById(R.id.back);
+        backButtonImageview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                backToMainActivity();
+            }
+        });
+    }
+
+    public void backToMainActivity(){
+        Intent intent = new Intent(ContactUs.this,MainActivity.class);
+        startActivity(intent);
     }
 }
